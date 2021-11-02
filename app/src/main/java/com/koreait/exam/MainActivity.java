10f2a2c1c94fc37b0bfa1f3c1f24e026bdf54d50
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-11-02 10:41:50 +0900

    계산기 완료

--- app/src/main/java/com/koreait/exam/MainActivity.java
+++ app/src/main/java/com/koreait/exam/MainActivity.java
@@ -8,29 +8,87 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etText;
+    private TextView tvDisplay;
+    private EditText etVal1;
+    private EditText etVal2;
+    private EditText etResult;
+
+    private String operator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etText = findViewById(R.id.etText);
+        tvDisplay = findViewById(R.id.tvDisplay);
+        etVal1 = findViewById(R.id.etVal1);
+        etVal2 = findViewById(R.id.etVal2);
+        etResult = findViewById(R.id.etResult);
     }
     // Clear 버튼 이벤트
     public void clkClear(View v) {
-        //etText.setText("");
-        etText.getText().clear();
+        operator = "";
+        tvDisplay.setText("");
+        etVal1.getText().clear();
+        etVal2.getText().clear();
+        etResult.getText().clear();
     }
 
-    // Clear 버튼 제외한 나머지 버튼들의 이벤트
-    public void clkBtn(View v) {
-        String str = etText.getText().toString();
-
+    // 숫자 버튼 이벤트
+    public void clkNumber(View v) {
         TextView tv = (TextView)v;
-        String val = tv.getText().toString();
+        String num = tv.getText().toString();
+        addTvDisplay(num);
+        switch (operator) {
+            case "":
+                etVal1.append(num);
+                break;
+            default:
+                String oldVal2 = etVal2.getText().toString();
+                etVal2.setText(oldVal2 + num);
+                break;
+        }
+    }
 
-        str += val;
+    // 사칙연산 버튼 이벤트
+    public void clkOperator(View v) {
+        TextView tv = (TextView)v;
+        String oper = tv.getText().toString();
+        operator = oper;
 
-        etText.setText(str);
+        addTvDisplay(oper);
+    }
+
+    // equal 버튼 이벤트
+    public void clkEqual(View v) {
+        addTvDisplay("=");
+        String strVal1 = etVal1.getText().toString();
+        String strVal2 = etVal2.getText().toString();
+        int intVal1 = Integer.parseInt(strVal1);
+        int intVal2 = Integer.parseInt(strVal2);
+
+        String strResult = "";
+
+        switch(operator) {
+            case "+":
+                strResult = String.valueOf(intVal1 + intVal2);
+                break;
+            case "-":
+                strResult = String.valueOf(intVal1 - intVal2);
+                break;
+            case "/":
+                strResult = String.valueOf((float)intVal1 / intVal2);
+                break;
+            case "*":
+                strResult = String.valueOf(intVal1 * intVal2);
+                break;
+        }
+        addTvDisplay(strResult);
+        etResult.setText(strResult);
+    }
+
+    private void addTvDisplay(String val) {
+        //String oldVal = tvDisplay.getText().toString();
+        //tvDisplay.setText(oldVal + val);
+        tvDisplay.append(val);
     }
 }
